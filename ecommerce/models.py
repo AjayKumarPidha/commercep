4dcb08db08dc6247674ebb105e92ffd73710e87e
AjayKumarPidha <ajaypidha408@gmail.com>
2022-09-13 23:15:04 +0530

    add push

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -8,6 +8,11 @@ class mindset(models.Model):
     name=models.CharField(max_length=100,blank=True,null=True)
     date=models.DateField(auto_created=True)
     heading=models.CharField(max_length=100,blank=True,null=True)
+    heading=models.CharField(max_length=100,blank=True,null=True)
+    heading=models.CharField(max_length=100,blank=True,null=True)
+    heading=models.CharField(max_length=100,blank=True,null=True)
+    heading=models.CharField(max_length=100,blank=True,null=True)
+    
     description1=models.CharField(max_length=100,blank=True,null=True)
     description2=models.CharField(max_length=100,blank=True,null=True)
     description3=models.CharField(max_length=100,blank=True,null=True)
