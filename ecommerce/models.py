13f5d819c25abebdcd488267ad1fc3cdc1ebea94
AjayKumarPidha <ajaypidha408@gmail.com>
2022-09-13 23:13:44 +0530

    add piush

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,4 @@
+from operator import mod
 from turtle import heading
 from django.db import models
 
@@ -7,3 +8,8 @@ class mindset(models.Model):
     name=models.CharField(max_length=100,blank=True,null=True)
     date=models.DateField(auto_created=True)
     heading=models.CharField(max_length=100,blank=True,null=True)
+    description1=models.CharField(max_length=100,blank=True,null=True)
+    description2=models.CharField(max_length=100,blank=True,null=True)
+    description3=models.CharField(max_length=100,blank=True,null=True)
+    description4=models.CharField(max_length=100,blank=True,null=True)
+    description5=models.CharField(max_length=100,blank=True,null=True)
