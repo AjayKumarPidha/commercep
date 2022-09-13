9185fd17c8e0a1a7d56073724c00547a7d5cfde7
AjayKumarPidha <ajaypidha408@gmail.com>
2022-09-13 22:39:39 +0530

    add push

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,9 @@
+from turtle import heading
 from django.db import models
 
 # Create your models here.
+
+class mindset(models.Model):
+    name=models.CharField(max_length=100,blank=True,null=True)
+    date=models.DateField(auto_created=True)
+    heading=models.CharField(max_length=100,blank=True,null=True)
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,9 @@
+import re
+from urllib import request
 from django.shortcuts import render
+from django import views
 
 # Create your views here.
+class com(views):
+     def get(self,request):
+        return render("hello")
